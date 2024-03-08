eedfd852a3cd4dd6e7b714e1f12199ca44a12061
İrem Akın Tunga <iremakin00@gmail.com>
2024-03-08 18:52:09 +0300

    completes tasks

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,7 +12,30 @@ public class Main {
 
     }
 
-    public static boolean checkForPalindrome(String text) {
+    public static boolean checkForPalindrome(String text) { System.out.println("initial text= " + text);
+        text = text.replaceAll("[.,'?\\-\\s]", "").toLowerCase(Locale.ENGLISH);
+
+        Stack<Character> stackOriginal = new Stack<>();
+        Stack<Character> stackTemp = new Stack<>();
+        Stack<Character> stackReversed = new Stack<>();
+
+        char[] chars = text.toCharArray();
+        for (char c : chars) {
+            stackOriginal.push(c);
+            stackTemp.push(c);
+        }
+
+        for (char c : chars) {
+            Character charTemp = stackTemp.pop();
+            stackReversed.push(charTemp);
+        }
+
+        System.out.println("reversed=" + stackReversed);
+        System.out.println("original=" + stackOriginal);
+
+        if (stackOriginal.equals(stackReversed)) {
+            return true;
+        }
         return false;
     }
 }
